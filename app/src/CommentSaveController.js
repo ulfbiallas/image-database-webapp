066bb95b84ea411321f485e8f4af073076eef674
ulfbiallas <ulfbiallas@googlemail.com>
2015-08-23 12:35:16 +0200

    added post request to save comment

--- app/src/CommentSaveController.js
+++ app/src/CommentSaveController.js
@@ -1,7 +1,18 @@
 var app = angular.module('image-database-webapp')
 
-app.controller('commentSaveController', function($scope, $http){
+app.controller('commentSaveController', function($scope, $http, $routeParams){
     $scope.submit = function() {
         console.log('Comment: ' + $scope.content)
+        console.log('Image: ' + $routeParams.imageId)
+
+        var comment = {
+            "content": $scope.content
+        }
+
+        var commentUrl = 'http://localhost:8080/image/'+$routeParams.imageId+'/comments'
+        $http.post(commentUrl, comment).then(function(response) {
+            console.log('status: ' + response.status)
+        })
+
     }
 })
